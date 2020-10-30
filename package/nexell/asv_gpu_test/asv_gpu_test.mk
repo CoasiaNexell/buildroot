6cf5fe25fb0577b0f433a8b385ca12fff5431c9d
Youngdo, Lee <ydlee@coasianexell.com>
2020-10-30 10:07:16 +0900

    nxp3220: SLT: to Add asv_gpu_test package to support 3D testsuite
    This patch add asv_gpu_test package to support 3D testsuite
    
    Signed-off-by: Youngdo, Lee <ydlee@coasianexell.com>
    Change-Id: I06499758b9b222f2fea106df399b9aad037510aa

---
+++ package/nexell/asv_gpu_test/asv_gpu_test.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# asv_gpu_test
+#
+################################################################################
+
+ASV_GPU_TEST_VERSION = 0.0.0
+ASV_GPU_TEST_SOURCE = gpu.tar.gz
+ASV_GPU_TEST_SITE = $(TOPDIR)/package/nexell/asv_gpu_test
+ASV_GPU_TEST_SITE_METHOD = file
+ASV_GPU_TEST_INSTALL_STAGING = YES
+ASV_GPU_TEST_INSTALL_TARGET = YES
+ASV_GPU_TEST_DEPENDENCIES = libnx_mali libnx_drm_allocator libnx_renderer libdrm_nexell
+
+ASV_GPU_TEST_CFLAGS += -I$(STAGING_DIR)/usr/include -I$(@D)/import/3rdparty/include/khronos -I$(@D)/import/include/EGL/platform_drm
+ASV_GPU_TEST_CXXFLAGS += -I$(STAGING_DIR)/usr/include -I$(@D)/import/3rdparty/include/khronos -I$(@D)/import/include/EGL/platform_fbdev -Wno-narrowing
+
+define ASV_GPU_TEST_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) SDKTARGETSYSROOT=$(STAGING_DIR) \
+		CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" CXXFLAGS="$(TARGET_CFLAGS) $(ASV_GPU_TEST_CXXFLAGS)" \
+	-C $(@D)
+endef
+
+define ASV_GPU_TEST_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/asv_graphic $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/share/asv_gpu_test
+	$(INSTALL) -D -m 0644 $(@D)/data/* $(TARGET_DIR)/usr/share/asv_gpu_test
+endef
+
+$(eval $(generic-package))
